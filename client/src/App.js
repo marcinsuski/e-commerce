cf76e2e005d2e1286e0312af7822f9fb3b396e1d
Marcin Suski <114236312+marcinsuski@users.noreply.github.com>
2023-01-31 17:15:24 +0100

    basic redux setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,10 @@
 import { useEffect } from "react";
 import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
-import Home from "./scenes/home/Home";
+import Home from "./pages/home/Home";
+import ItemDetails from "./pages/ItemDetails/ItemDetails";
+import Checkout from "./pages/checkout/Checkout";
+import Confirmation from "./pages/checkout/Confirmation";
+import Navbar from "./pages/global/Navbar";
 
 const ScrollToTop = () => {
     const { pathname } = useLocation();
@@ -15,9 +19,16 @@ function App() {
     return (
         <div className="app">
             <BrowserRouter>
+                <Navbar />
                 <ScrollToTop />
                 <Routes>
                     <Route path="/" element={<Home />} />
+                    <Route path="/item/:itemId" element={<ItemDetails />} />
+                    <Route path="/checkout" element={<Checkout />} />
+                    <Route
+                        path="/checkout/success"
+                        element={<Confirmation />}
+                    />
                 </Routes>
             </BrowserRouter>
         </div>
